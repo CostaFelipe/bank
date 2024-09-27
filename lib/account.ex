a3fdf836b12e315ff880073908d7de038555c9db
wedo <costadefelipe@gmail.com>
2024-09-27 10:33:30 -0300

    add new functions search_account & search_account_email

--- lib/account.ex
+++ lib/account.ex
@@ -1,18 +1,35 @@
 defmodule Bank.Account do
 
   defstruct user: User, balance: 1000
+  @contas "account.txt"
 
-  def new(user), do: %__MODULE__{user: user}
+  def new(user) do
+    case  search_accounts_email(user.email) do
+      nil ->
+      binary = [%__MODULE__{user: user}]  ++ search_accounts()
+      |> :erlang.term_to_binary()
+      File.write(@contas, binary)
+      _ -> {:error, "account registered!"}
+    end
+  end
 
-  def transferring(accounts, send, to, value) do
-    send = Enum.find(accounts, fn account -> account.user.email == send.user.email end)
+  def search_accounts do
+      {:ok, binary} = File.read(@contas)
+      :erlang.binary_to_term(binary)
+  end
+
+  def search_accounts_email(email), do: Enum.find(search_accounts(), &(&1.user.email == email))
+
+  def transferring(send, to, value) do
+    send = search_accounts_email(send.user.email)
 
     cond do
       valid_balance(send.balance, value) -> {:error, "insufficient balance"}
       true ->
-        to = Enum.find(accounts, fn account -> account.user.email == to.user.email end)
-        send = %Bank.Account{ send | balance: send.balance - value}
-      [to, send]
+        search_accounts()
+      #   to =  search_accounts_email(to.user.email)
+      #   send = %Bank.Account{ send | balance: send.balance - value}
+      # [to, send]
     end
   end
 
