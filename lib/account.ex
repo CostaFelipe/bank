c4c35c769b4fe637de49a923dc34826608f3b41d
wedo <costadefelipe@gmail.com>
2024-10-28 19:35:22 -0300

    add news structs and functions

--- lib/account.ex
+++ lib/account.ex
@@ -1,3 +1,8 @@
 defmodule Account do
+  defstruct users: []
 
+  def insert(users, name, email) do
+    new_user = User.new(name, email)
+    [new_user | users]
+  end
 end
--- lib/user.ex
+++ lib/user.ex
@@ -1,3 +1,11 @@
 defmodule User do
 
+  defstruct name: nil, email: nil, balance: 1000
+
+  def new(name, email) do
+    %__MODULE__{
+      name: name,
+      email: email
+    }
+  end
 end
