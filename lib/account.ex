46f18569ae38caeae41de355f04ffdd347ca5bd2
wedo <costadefelipe@gmail.com>
2024-10-29 13:29:47 -0300

    add new tests user & accounts and refactory fuction

--- lib/account.ex
+++ lib/account.ex
@@ -2,7 +2,9 @@ defmodule Account do
   defstruct users: []
 
   def insert(users, name, email) do
-    new_user = User.new(name, email)
-    [new_user | users]
+    case Enum.find(users, fn user -> user.email == email end) do
+      nil -> [User.new(name, email) | users]
+      _ -> users
+    end
   end
 end
--- test/account_test.exs
+++ test/account_test.exs
@@ -1,11 +1,18 @@
 defmodule AccountTest do
   use ExUnit.Case
 
-  test "Given name, email, when insert, then return a new user list" do
-    users = []
-    users = Account.insert(users, "Jhon doe", "jon@doe.com")
-    users = Account.insert(users, "Ada", "ada@ada.com")
-    assert length(users) == 2
-  end
+  describe "insert/3" do
+    test "Given name, email, when insert, then return a new user list" do
+      users = []
+      users = Account.insert(users, "Jhon doe", "jon@doe.com")
+      users = Account.insert(users, "Ada", "ada@ada.com")
+      assert length(users) == 2
+    end
 
+    test "Give a user that already exist, when insert, then return the same user list" do
+      users = []
+      users = users |> Account.insert("Jhon doe", "jon@doe.com") |> Account.insert("Jhon doe", "jon@doe.com")
+      assert length(users) == 1
+    end
+  end
 end
